b7f000653bb0b8f0eb989a4f0a31d75383e56783
Sumit.rawal <ersumitrawal658@gmail.com>
2020-12-08 15:27:34 +0700

    Adding new files

---
+++ src/CountOccurenceOfEachCharacterUsing1ForLoop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class occurenceOfCharacter {
+    public static void main(String[] args) {
+        String str = "SSDRRRTTYYTYTR";
+        HashMap <Character, Integer> hMap = new HashMap<>();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (hMap.containsKey(str.charAt(i))) {
+                int count = hMap.get(str.charAt(i));
+                hMap.put(str.charAt(i), ++count);
+            } else {
+                hMap.put(str.charAt(i),1);
+            }
+        }
+        System.out.println(hMap);
+    }
+}
